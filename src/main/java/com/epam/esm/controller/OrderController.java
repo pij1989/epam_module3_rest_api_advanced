864f3ce5199b7f7aca8c4f84d3bbbba04c5ede2a
Dmitri Pozharsky <dmitri.pozharsky@gmail.com>
2021-06-27 19:46:17 +0300

    added changes to order controller

--- src/main/java/com/epam/esm/controller/OrderController.java
+++ src/main/java/com/epam/esm/controller/OrderController.java
@@ -1,14 +1,22 @@
 package com.epam.esm.controller;
 
+import com.epam.esm.model.entity.Order;
 import com.epam.esm.model.hateoas.assembler.OrderModelAssembler;
+import com.epam.esm.model.hateoas.model.OrderModel;
 import com.epam.esm.model.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/orders", produces = {MediaType.APPLICATION_JSON_VALUE, MediaTypes.HAL_JSON_VALUE})
@@ -21,5 +29,18 @@ public class OrderController {
     public OrderController(OrderService orderService, OrderModelAssembler orderModelAssembler) {
         this.orderService = orderService;
         this.orderModelAssembler = orderModelAssembler;
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderModel> createOrder(@RequestBody Order order) {
+        logger.debug("ORDER: " + order);
+        Optional<Order>optionalOrder = orderService.createOrder(order);
+        if (optionalOrder.isPresent()) {
+            Order createdOrder = optionalOrder.get();
+            OrderModel orderModel = orderModelAssembler.toModel(createdOrder);
+            return new ResponseEntity<>(orderModel, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
